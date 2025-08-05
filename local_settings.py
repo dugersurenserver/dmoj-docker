b865ac7ba62f2bdcbebfbd0814413a8d5ce61fa5
dugersurenserver <127100644+dugersurenserver@users.noreply.github.com>
2025-08-05 13:12:39 +0800

    Update local_settings.py
    hi

--- local_settings.py
+++ local_settings.py
@@ -18,7 +18,7 @@ HOST = os.environ.get('HOST', '*')
 
 # Uncomment and set to the domain names this site is intended to serve.
 # You must do this once you set DEBUG to False.
-ALLOWED_HOSTS = [HOST]
+ALLOWED_HOSTS = ['*']
 
 # Optional apps that DMOJ can make use of.
 INSTALLED_APPS += ()
